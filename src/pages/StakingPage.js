3e6e91164da66ea2fccb7d5a1e559448128ce677
Sam Ayorinde <41350405+Sam-Devs@users.noreply.github.com>
2020-10-09 00:00:52 +0100

    new updates

--- src/pages/StakingPage.js
+++ src/pages/StakingPage.js
@@ -362,7 +362,7 @@ const HomePage = () => {
                 {!loading && (error !== "" ? error : "CONNECT METAMASK WALLET")}
               </Button>
 
-              <div className="text-white text-center mt-6 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+              <div className="text-white text-center mt-6 text-base sm:text-lg md:text-xl lg:text-2xl xl:text-3xl font-bold">
               <h1>LEAD STAKING</h1>
               </div>
               <div className="w-full md:w-3/6 justify-center mx-auto mt-6">  
